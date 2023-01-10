64fb9b80bc1a6112eae336021103177fa9b9c508
Michael Dougherty <maackle.d@gmail.com>
2023-01-10 14:35:06 -0800

    Use channel to receive outcomes

--- src/manager.rs
+++ src/manager.rs
@@ -10,28 +10,35 @@ use std::{
     hash::Hash,
 };
 
-use futures::{stream::FuturesUnordered, Future, FutureExt, Stream, StreamExt};
+use futures::{
+    channel::mpsc, future::BoxFuture, stream::FuturesUnordered, Future, FutureExt, Stream,
+    StreamExt,
+};
 
 use crate::{signal::StopListener, StopBroadcaster, Task};
 
 /// Tracks tasks at the global conductor level, as well as each individual cell level.
 pub struct TaskManager<GroupKey, Outcome> {
-    groups: HashMap<GroupKey, TaskGroup<Outcome>>,
+    groups: HashMap<GroupKey, TaskGroup>,
     children: HashMap<GroupKey, HashSet<GroupKey>>,
     parent_map: Box<dyn 'static + Send + Sync + Fn(&GroupKey) -> Option<GroupKey>>,
+    outcomes: mpsc::Sender<(GroupKey, Outcome)>,
 }
 
 impl<GroupKey, Outcome> TaskManager<GroupKey, Outcome>
 where
-    GroupKey: Clone + Eq + Hash,
+    GroupKey: Clone + Eq + Hash + Send + 'static,
+    Outcome: Send + 'static,
 {
     pub fn new(
+        outcomes: mpsc::Sender<(GroupKey, Outcome)>,
         parent_map: impl 'static + Send + Sync + Fn(&GroupKey) -> Option<GroupKey> + 'static,
     ) -> Self {
         Self {
             groups: Default::default(),
             children: Default::default(),
             parent_map: Box::new(parent_map),
+            outcomes,
         }
     }
 
@@ -39,10 +46,17 @@ where
     pub fn add_task<Fut: Future<Output = Outcome> + Send + 'static>(
         &mut self,
         key: GroupKey,
-        f: impl FnOnce(StopListener) -> Fut,
+        f: impl FnOnce(StopListener) -> Fut + Send + 'static,
     ) {
-        let group = self.group(key);
-        group.tasks.push(f(group.stopper.listener()).boxed());
+        let mut tx = self.outcomes.clone();
+        let group = self.group(key.clone());
+        let listener = group.stopper.listener();
+        let task = async move {
+            let outcome = f(listener).await;
+            tx.try_send((key, outcome)).ok();
+        }
+        .boxed();
+        group.tasks.push(task);
     }
 
     pub fn num_tasks(&self, key: &GroupKey) -> usize {
@@ -54,16 +68,17 @@ where
 
     /// Remove a group, returning the group as a stream which produces
     /// all task results in the order they resolve.
-    pub fn stop_group(&mut self, key: &GroupKey) -> impl Stream<Item = (GroupKey, Outcome)> {
-        let mut stream = futures::stream::SelectAll::new();
+    pub fn stop_group(&mut self, key: &GroupKey) -> GroupStop {
+        let mut tasks = vec![];
         for key in self.descendants(key) {
             if let Some(mut group) = self.groups.remove(&key) {
                 // Signal all tasks to stop.
                 group.stopper.emit();
-                stream.push(group.tasks.map(move |info| (key.clone(), info)));
+                tasks.push(group.tasks.collect::<Vec<_>>());
             }
         }
-        stream
+
+        futures::future::join_all(tasks).map(|_| ()).boxed()
     }
 
     pub(crate) fn descendants(&self, key: &GroupKey) -> HashSet<GroupKey> {
@@ -81,7 +96,7 @@ where
         all
     }
 
-    fn group(&mut self, key: GroupKey) -> &mut TaskGroup<Outcome> {
+    fn group(&mut self, key: GroupKey) -> &mut TaskGroup {
         self.groups.entry(key.clone()).or_insert_with(|| {
             if let Some(parent) = (self.parent_map)(&key) {
                 self.children
@@ -94,12 +109,14 @@ where
     }
 }
 
-struct TaskGroup<Outcome> {
-    pub(crate) tasks: FuturesUnordered<Task<Outcome>>,
+pub type GroupStop = BoxFuture<'static, ()>;
+
+struct TaskGroup {
+    pub(crate) tasks: FuturesUnordered<Task>,
     pub(crate) stopper: StopBroadcaster,
 }
 
-impl<Outcome> TaskGroup<Outcome> {
+impl TaskGroup {
     pub fn new() -> Self {
         Self {
             tasks: FuturesUnordered::new(),
@@ -113,6 +130,7 @@ pub type TaskStream<GroupKey, Outcome> =
 
 #[cfg(test)]
 mod tests {
+    use futures::channel::mpsc;
     use maplit::hashset;
     use rand::seq::SliceRandom;
 
@@ -134,7 +152,8 @@ mod tests {
     #[tokio::test]
     async fn test_descendants() {
         use GroupKey::*;
-        let mut tm: TaskManager<GroupKey, String> = TaskManager::new(|g| match g {
+        let (tx, outcomes) = mpsc::channel(1);
+        let mut tm: TaskManager<GroupKey, String> = TaskManager::new(tx, |g| match g {
             A => None,
             B => Some(A),
             C => Some(B),
@@ -148,8 +167,10 @@ mod tests {
         keys.shuffle(&mut rand::thread_rng());
 
         // Set up the parent map in random order
-        for key in keys {
-            tm.add_task(key, |_| map_jh(tokio::spawn(async { "".to_string() })))
+        for key in keys.clone() {
+            tm.add_task(key.clone(), |_| {
+                map_jh(tokio::spawn(async move { format!("{:?}", key) }))
+            })
         }
 
         assert_eq!(tm.descendants(&A), hashset! {A, B, C, D, E, F, G});
@@ -159,26 +180,34 @@ mod tests {
         assert_eq!(tm.descendants(&E), hashset! {E, F});
         assert_eq!(tm.descendants(&F), hashset! {F});
         assert_eq!(tm.descendants(&G), hashset! {G});
+
+        tm.stop_group(&A).await;
+
+        assert_eq!(
+            outcomes.take(keys.len()).collect::<HashSet<_>>().await,
+            hashset! {
+                (A, "A".to_string()),
+                (B, "B".to_string()),
+                (C, "C".to_string()),
+                (D, "D".to_string()),
+                (E, "E".to_string()),
+                (F, "F".to_string()),
+                (G, "G".to_string()),
+            }
+        );
     }
 
     #[tokio::test]
     async fn test_group_nesting() {
         use GroupKey::*;
-        let mut tm: TaskManager<GroupKey, String> = TaskManager::new(|g| match g {
+        let (tx, mut outcomes) = mpsc::channel(1);
+        let mut tm: TaskManager<GroupKey, String> = TaskManager::new(tx, |g| match g {
             A => None,
             B => Some(A),
             C => Some(B),
             D => Some(B),
             _ => None,
         });
-
-        // async fn collect<GroupKey: Hash + Eq, Outcome: Hash + Eq>(
-        //     stream: impl Stream<Item = (GroupKey, Outcome)>,
-        // ) -> HashSet<(GroupKey, Outcome)> {
-        //     let infos: Vec<_> = stream.collect().await;
-        //     let infos: Result<HashSet<_>, _> = infos.into_iter().collect();
-        //     infos.unwrap()
-        // }
 
         tm.add_task(A, |stop| blocker("a1", stop));
         tm.add_task(A, |stop| blocker("a2", stop));
@@ -193,8 +222,9 @@ mod tests {
 
         // let infos: Vec<_> = tm.stop_group(&D).collect().await;
         // let infos: Result<Vec<_>, _> = infos.into_iter().collect();
+        tm.stop_group(&D).await;
         assert_eq!(
-            tm.stop_group(&D).collect::<HashSet<_>>().await,
+            hashset![outcomes.next().await.unwrap(),],
             hashset![(D, "d1".to_string())]
         );
 
@@ -206,8 +236,13 @@ mod tests {
         tm.add_task(D, |stop| blocker("dx", stop));
         assert_eq!(tm.num_tasks(&D), 1);
 
+        tm.stop_group(&B).await;
         assert_eq!(
-            tm.stop_group(&B).collect::<HashSet<_>>().await,
+            hashset![
+                outcomes.next().await.unwrap(),
+                outcomes.next().await.unwrap(),
+                outcomes.next().await.unwrap(),
+            ],
             hashset![
                 (B, "b1".to_string()),
                 (C, "c1".to_string()),
@@ -223,8 +258,13 @@ mod tests {
         tm.add_task(D, |stop| blocker("dy", stop));
         assert_eq!(tm.num_tasks(&D), 1);
 
+        tm.stop_group(&A).await;
         assert_eq!(
-            tm.stop_group(&A).collect::<HashSet<_>>().await,
+            hashset![
+                outcomes.next().await.unwrap(),
+                outcomes.next().await.unwrap(),
+                outcomes.next().await.unwrap(),
+            ],
             hashset![
                 (A, "a1".to_string()),
                 (A, "a2".to_string()),
